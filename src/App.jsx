92625a127857cc3f45680bfa7d6be7c8761db51a
Aleksandr Khachich <95611562+Khanchic@users.noreply.github.com>
2023-02-15 13:18:10 +0200

    mark up

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,8 @@
-import { Component } from 'react';
-import { NotificationContainer, NotificationManager } from 'react-notifications';
-import 'react-notifications/lib/notifications.css';
+import { useState, useEffect } from 'react';
+import Notiflix from 'notiflix';
 
 import { FetchImagesByQuery } from './services/FetchImagesByQuery';
-import { STATUS } from './constants/status.constants';
+
 
 import { Searchbar } from './components/Searchbar/Searchbar';
 import { ImageGallery } from './components/ImageGallery/ImageGallery';
@@ -11,143 +10,108 @@ import { Loder } from './components/Loder/Loder';
 import { Button } from './components/Button/Button';
 import {Modal } from './components/Modal/Modal';
 
-export class App extends Component {
-  state = {
-    images: [],
-    largeImage: '',
-    page: 1,
-    query: '',
-    isLoading: false,
-    imagesPerPage: 12,
-    showLoadMoreBtn: false,
-    status: STATUS.idle,
-    showModal: false,
-
-  };
 
 
-  componentDidUpdate(_, prevState) {
 
-    const { query, page } = this.state;
+export const App = () => {
+  const [images, setImages] = useState([]);
+  const [largeImage, setLargeImage] = useState('');
+  const [page, setPage] = useState(0);
+  const [query, setQuery] = useState('');
+  const [isLoading, setIsLoading] = useState(false);
+  const [showLoadMoreBtn, setShowLoadMoreBtn] = useState(false);
+  const [showModal, setShowModal] = useState(false);
 
-    if (
-      prevState.page !== page ||
-      prevState.query !== query
-    ) {
-      this.setState({ isLoading: true });
-      const response = FetchImagesByQuery(query, page);
-      response
-        .then(({ data }) => {
-          const totalPages = Math.ceil(data.totalHits / this.state.imagesPerPage);
+
+  const imagesPerPage =12;
+
+
+  useEffect(() => {
+
+     if (!page) {
+      return;
+    }
+    setIsLoading(true);
+    const response = FetchImagesByQuery(query, page);
+    response
+      .then(({ data }) => {
+        const totalPages = Math.ceil(data.totalHits / imagesPerPage);
           
-            const imagesData = data.hits.map(
-            ({ id, webformatURL, largeImageURL,tags}) => ({
-              id,
-              webformatURL,
-              largeImageURL,
-              tags,
-            })
-          );
-          this.setState(({ images }) => ({
-            images: [...images, ...imagesData],
-            totalImages: data.totalHits,
-          }));
-          if (totalPages === 0) {
-            NotificationManager.warning('Sorry, there are no images matching your search query. Please try again') ;
+        const imagesData = data.hits.map(
+          ({ id, webformatURL, largeImageURL, tags }) => ({
+            id,
+            webformatURL,
+            largeImageURL,
+            tags,
+          })
+        );
+        setImages(images => [...images, ...imagesData]);
+          
+        if (totalPages === 0 ) {
+          Notiflix.Notify.info('Please, query what you want to see');
             
-          }
-          if (totalPages > page) {
-            this.setState({ showLoadMoreBtn: true });
+        }
+        if (totalPages > page) {
+          setShowLoadMoreBtn(true);
            
-          } else { this.setState({ showLoadMoreBtn: false }); }
-        })
+        } else { setShowLoadMoreBtn(false); }
+      })
           
-        .catch(() => { NotificationManager.error('Oops ...something went wrong') })
-        .finally(() => {
-          this.setState({ isLoading: false })
-        });
-    }
-  };
-    
-
+      .catch(() => { Notiflix.Notify.failure('Oops ...something went wrong') })
+      .finally(() => {
+        setIsLoading(false);
+      });
 
     
-  fetchData = async ({ query = '', page = 1 }) => {
-    this.setState({ isLoading: true });
-    try {
-      const { data } = await FetchImagesByQuery({ query, page });
-      const { hits } = data;
-      this.setState({ images: hits, status: STATUS.success });
-    } catch (error) {
-      this.setState({ status: STATUS.error });
+  }, [page, query]);
+
+
+
+      const handleUpdateQuery = value => {
+        if (value.trim().toLowerCase()) {
+          setQuery(value);
+          setPage(1);
+          setImages([]);
+          setShowLoadMoreBtn(false);
+        }
     }
-  };
-
-
-    handleUpdateQuery = value => {
-
-      const currentQuery = value.trim().toLowerCase();
-
-      if (currentQuery) {
-        this.setState({
-          page: 1,
-          query: value,
-          images: [],
-          isLoading: true,
-          showLoadMoreBtn: false,
-        
-        })
+  
+    
+    const handleLoadMore = () => {
+        setPage(page + 1);
       }
-    }
-
-
-    handleLoadMore = () => {
-      this.setState(prevState => ({
-        page: prevState.page + 1,
-        isLoading: true,
-
-       
-      }));
-    }
   
-    openModal = index => {
-      this.setState(({ images }) => ({
-        showModal: true,
-        largeImage: images[index].largeImageURL,
-      }));
+    const openModal = index => {
+      setShowModal(true);
+      setLargeImage(images[index].largeImageURL);
+      
     };
   
-    handleToggleModal = () => {
-    this.setState(prevState => ({ showModal: !prevState.showModal }));
+    const handleToggleModal = () => {
+    setShowModal(!showModal);
     };
 
-    render() {
+  return (
+    <>
+    <Searchbar onSubmit={handleUpdateQuery} />
 
-      const { isLoading, images, showModal, largeImage } = this.state;
-      const { handleUpdateQuery, handleToggleModal, openModal, handleLoadMore } = this;
-    
-      return (
-        <>
-          <Searchbar onSubmit={handleUpdateQuery} />
+    {images.length !== 0 && query!=='' && (
+    <ImageGallery images={images} openModal={openModal} />
+     )}
 
-          {images.length !== 0 && (
-          <ImageGallery images={images} openModal={openModal} />
-          )}
+    {showModal && (
+    <Modal onClose={handleToggleModal} largeImage={largeImage} />
+    )}
 
-          {showModal && (
-          <Modal onClose={handleToggleModal} largeImage={largeImage} />
-          )}
+    {isLoading && <Loder />}
 
-          {isLoading && <Loder />}
-
-          {this.state.showLoadMoreBtn && <Button onClick={handleLoadMore} />}
+    {showLoadMoreBtn && <Button onClick={handleLoadMore} />}
        
        
-          <NotificationContainer />
-        </>
+    </>     
     
-      );
+  );
  
-    }
-  
+
+
 }
--- src/components/Loder/Loder.js
+++ src/components/Loder/Loder.js
@@ -7,8 +7,8 @@ export const  Loder = ()=> {
     return (
       <div className={styles.container}>
             <RotatingLines
-                strokeColor="green"
-                strokeWidth="5"
+                strokeColor="red"
+                strokeWidth="3.5"
                 animationDuration="0.75"
                 width="96"
                 visible={true}
